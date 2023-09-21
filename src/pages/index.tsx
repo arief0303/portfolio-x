377accb6b8ba854b683aaf423eade03c38a65dec
arief0303 <arief0303@gmail.com>
2023-09-21 14:33:18 +0700

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,190 +1,252 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import { useRef, useEffect, useState, useMemo } from 'react'
+import { useFrame, useLoader, extend, useThree } from '@react-three/fiber'
+import { OrbitControls, useGLTF } from '@react-three/drei'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { GlobalCanvas, ViewportScrollScene, ScrollScene, UseCanvas, SmoothScrollbar, useTracker } from '@14islands/r3f-scroll-rig'
+import { PivotControls, MeshTransmissionMaterial, Grid, Environment, PerspectiveCamera, CameraControls, Text, Text3D, useTexture } from '@react-three/drei'
+import * as THREE from 'three'
+import { Model } from '../components/Untitled'
+import myFont from '../assets/fonts/XYBER_Regular.json'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
+import '@14islands/r3f-scroll-rig/css'
+import { useTrackerMotionValue } from '../components/useTrackerMotionValue'
+import { motion, useTransform } from 'framer-motion'
+import { WaveMaterial } from '../components/WaveMaterial'
+import { easing } from 'maath'
+import axios from "axios";
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+extend({ TextGeometry })
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
+const ShaderPlane = ({ vertex, fragment }) => {
+  const meshRef = useRef();
+
+  // Load the noise texture and update the shader uniform
+  const noiseTexture = useTexture("noise2.png");
+  useFrame((state) => {
+    let time = state.clock.getElapsedTime();
+
+    // start from 20 to skip first 20 seconds ( optional )
+    meshRef.current.material.uniforms.iTime.value = time + 20;
+  });
+
+  // Define the shader uniforms with memoization to optimize performance
+  const uniforms = useMemo(
+    () => ({
+      iTime: {
+        type: "f",
+        value: 1.0,
+      },
+      iResolution: {
+        type: "v2",
+        value: new THREE.Vector2(4, 3),
+      },
+      iChannel0: {
+        type: "t",
+        value: noiseTexture,
+      },
+    }),
+    []
+  );
+
+  return (
+    <mesh ref={meshRef}>
+      <planeGeometry args={[4, 3]} />
+      <shaderMaterial
+        uniforms={uniforms}
+        vertexShader={vertex}
+        fragmentShader={fragment}
+        side={THREE.DoubleSide}
+      />
+    </mesh>
+  );
+};
+
+const ViewportDemo = () => {
+  const el = useRef()
+  return (
+    <>
+      <section ref={el} className="TitleArea Debug">
+        <div ref={el} className="Placeholder ViewportScrollScene" style={{ touchAction: 'pan-x' }}></div>
+        <UseCanvas>
+          <ViewportDemoWebGL el={el} />
+        </UseCanvas>
+      </section>
+    </>
+  )
 }
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
+const ViewportDemoWebGL = ({ el }) => {
+  // State variables to store the vertex and fragment shaders as strings
+  const [vertex, setVertex] = useState("");
+  const [fragment, setFragment] = useState("");
+
+  // Fetch the shaders once the component mounts
+  useEffect(() => {
+    // fetch the vertex and fragment shaders from public folder 
+    axios.get("/vertexShader.glsl").then((res) => setVertex(res.data));
+    axios.get("/fragmentShader.glsl").then((res) => setFragment(res.data));
+  }, []);
+
+  // If the shaders are not loaded yet, return null (nothing will be rendered)
+  if (vertex == "" || fragment == "") return null;
+
+  return (
+    /* Disable hideOffscreen to avoid jank */
+    <ViewportScrollScene track={el} hideOffscreen={false}>
+      {(props) => (
+        <>
+          <mesh position-y={0.5}>
+            {/* <boxGeometry /> */}
+            <TitleText />
+
+            <ShaderPlane vertex={vertex} fragment={fragment} />
+
+            <Model rotation={[0, 180, 0]} scale={0.4} />
+
+            <MeshTransmissionMaterial
+              chromaticAberration={1}
+              thickness={0.3}
+              transmission={1}
+              anisotropy={0.5}
+              distortion={5}
+              distortionScale={1.5}
+              temporalDistortion={0.1}
+              metalness={0.1}
+              backside
+              resolution={256}
+              backsideResolution={256}
+            />
+          </mesh>
+          <Environment preset="dawn" />
+          <PerspectiveCamera fov={14} position={[0, 0, 20]} makeDefault onUpdate={(self) => self.lookAt(0, 0, 0)} />
+          {/* OrbitControls add touchAction='none' to the canvas eventSource and never removes it after events.connected changes it 
+              - need to manually pass in tracked domElement to keep touch scrolling working */}
+          {/* <OrbitControls domElement={props.track.current} makeDefault enableZoom={false} /> */}
+          {/* <CameraControls makeDefault /> */}
+        </>
+      )}
+    </ViewportScrollScene>
+  )
 }
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
+const TitleText = ({ el }) => {
+  let size
+  let font = "/fonts/XYBER - Regular.otf"
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
+  //if on mobile, resize text
+  if (window.innerWidth < 600) {
+    size = 0.35
+  } else {
+    size = 0.7
   }
-]
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
+  return <Text font={font} fontSize={size}>Catalyst</Text>
 }
 
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+/* function ShaderPlane() {
+  const ref = useRef()
+  const { viewport, size } = useThree()
+  useFrame((state, delta) => {
+    ref.current.time += delta
+    easing.damp3(ref.current.pointer, state.pointer, 0.2, delta)
+  })
+  return (
+    <mesh scale={[viewport.width * 2, viewport.height * 2, 1]}>
+      <planeGeometry />
+      <waveMaterial ref={ref} key={WaveMaterial.key} resolution={[size.width * viewport.dpr, size.height * viewport.dpr]} />
+    </mesh>
+  )
+} */
+
+function HorizontalMarquee() {
+  const el = useRef()
+  const tracker = useTracker(el)
+  const progress = useTrackerMotionValue(tracker)
+
+  const x = useTransform(progress, [0, 1], ['0vw', '-50vw'])
+
+  return (
+    <section ref={el} className="Marquee Debug">
+      <motion.div style={{ x }}>
+        <h1>TEXT BIG TEXT BIG TEXT</h1>
+      </motion.div>
+    </section>
+  )
+}
+
+function VerticalParallax({ children }) {
+  const el = useRef()
+  const tracker = useTracker(el)
+  const progress = useTrackerMotionValue(tracker)
+
+  const textY = useTransform(progress, [0, 1], ['25%', '-25%'])
+  const imageY = useTransform(progress, [0, 1], ['-25vh', '25vh'])
+
+  return (
+    <section ref={el} className="VerticalParallax Debug">
+      <motion.div className="VerticalParallaxMotion" style={{ y: textY }}>
+        <h2>{children}</h2>
+      </motion.div>
+      <motion.div className="Image" style={{ y: imageY }}></motion.div>
+    </section>
+  )
+}
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+   <>
+   <SmoothScrollbar>
+        {(bind) => (
+          <article {...bind}>
+            <ViewportDemo />
+
+            {/* <header>@14islands/r3f-scroll-rig + Framer Motion</header> */}
+
+            <section>
+              {/* <h1>HTML parallax with useTracker() and Framer Motion</h1> */}
+            </section>
+
+            <section>
+              {/* <p>
+                The <code>useTracker()</code> can be used by regular HTML components to get their progress through the viewport.
+              </p> */}
+            </section>
+
+            {/* <section>&nbsp;</section> */}
+
+
+            <VerticalParallax>Vertical Text</VerticalParallax>
+
+            <section>
+              <p>In this example, we take the scroll progress from the tracker and feed it into a MotionValue.</p>
+            </section>
+
+            <HorizontalMarquee>R3F Scroll Rig</HorizontalMarquee>
+
+            <section>&nbsp;</section>
+            {/* <header>
+              <h1>Project Catalyst</h1>
+            </header>
+            <section>
+              <h1>Basic &lt;ScrollScene/&gt; example</h1>
+            </section> */}
+            {/* {isTouch && (
+              <section>
+                <p style={{ color: 'orange' }}>
+                  You are on a touch device which means the WebGL won't sync with the native scroll. Consider disabling ScrollScenes for
+                  touch devices, or experiment with the `smoothTouch` setting on Lenis.
+                </p>
+              </section>
+            )}
+            <section>Both these ScrollScenes are tracking DOM elements and scaling their WebGL meshes to fit.</section> */}
+
+            <section>&nbsp;</section>
+          </article>
+        )}
+      </SmoothScrollbar>
+   </>
   )
 }
 
