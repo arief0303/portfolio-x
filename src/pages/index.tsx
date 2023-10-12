b82a951fb5017f51ce932e94f1cf767b08017d8b
Arief Raihan <arief0303@gmail.com>
2023-10-12 19:06:04 +0700

    favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -685,4 +685,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Portfolio</title>
