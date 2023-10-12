b64c68550f56dc60430e97c374085c352341bedd
Arief Raihan <arief0303@gmail.com>
2023-10-12 15:08:08 +0700

    Carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,6 +23,9 @@ import { motion, useTransform } from 'framer-motion'
 import { a, useTransition } from "@react-spring/web";
 import { easing } from 'maath'
 import axios from "axios";
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
+// import { Carousel, Typography, Button } from "@material-tailwind/react";
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
 extend({ TextGeometry })
 
@@ -66,8 +69,73 @@ const WordFade = ({ words = [], duration = 3000 }: { words: string[]; duration: 
     </div>
   );
 };
+const Carousel = () => {
+  const slides = [
+    {
+      url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+    },
 
-export const AnimatedText = () => {
+    {
+      url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = ({ slideIndex }: { slideIndex: number }) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  return (
+    <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+      ></div>
+      {/* Left Arrow */}
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+      <div className='flex top-4 justify-center py-2'>
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className='text-2xl cursor-pointer'
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+const AnimatedText = () => {
   /* const [fadeProp, setFadeProp] = useState<FadeProp>({ fade: 'fade-in' })
   const [wordOrder, setWordOrder] = useState(0)
 
@@ -353,7 +421,7 @@ function AboutSection({ id, src, ...props }: { id: string, src: string }) {
       if (window.innerWidth < 600) {
         // setRatio("5vw");
         // document.getElementById("SphereViewport")!.style.zIndex = "-10";
-        
+
       } else {
         // setRatio("2vw");
       }
@@ -590,11 +658,13 @@ const IndexPage: React.FC<PageProps> = () => {
               <img src="/images/image2.png"></img>
               <br />
               <img src="/images/image3.png"></img> */}
-                <img src={image2} />
+                {/* <img src={image2} />
                 <br />
                 <img src={image3} />
                 <br />
-                <img src={image4} />
+                <img src={image4} /> */}
+
+                <Carousel />
               </section>
               {/* <section>&nbsp;</section> */}
             </article>
