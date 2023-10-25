824c8cccdd5dd96b13e39f183ae4d075352f72d7
Arief R Syauqie <arief0303@gmail.com>
2023-10-26 03:45:33 +0700

    desc fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,15 +74,15 @@ const Carousel = () => {
   const slides = [
     {
       url: '/images/image1.png',
-      description: 'Bitaverse',
-    },
-    {
-      url: '/images/screenshot1.png',
       description: 'Aspace',
     },
     {
+      url: '/images/screenshot1.png',
+      description: 'Bitaverse',
+    },
+    {
       url: '/images/image3.png',
-      description: '3D Van interior configurator.',
+      description: '3D Van interior configurator',
     },
     /* {
       url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
